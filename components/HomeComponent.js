4ce5e7d315e33864279314cac2f23ce6fac50884
Luis Locon <luis.locon@gmail.com>
2020-07-24 18:51:56 -0600

    [Adding]: Adding more views for Home Screen

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { ScrollView, View, Text } from "react-native";
+import { Card } from "react-native-elements";
+import { DISHES } from "../shared/dishes";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 
-export default function Home() {
+function RenderItem({ item }) {
+  if (item != null) {
+    return (
+      <Card
+        featuredTitle={item.name}
+        featuredSubtitle={item.designation}
+        image={require("./images/uthappizza.png")}
+      >
+        <Text style={{ margin: 10 }}>{item.description}</Text>
+      </Card>
+    );
+  }
+
+  return <View></View>;
+}
+
+export default function Home(props) {
+  const [dishes] = useState(DISHES);
+  const [promotions] = useState(PROMOTIONS);
+  const [leaders] = useState(LEADERS);
+
   return (
-    <View>
-      <Text>Home Component</Text>
-    </View>
-  )
+    <ScrollView>
+      <RenderItem item={dishes.filter((dish) => dish.featured)[0]} />
+      <RenderItem
+        item={promotions.filter((promotion) => promotion.featured)[0]}
+      />
+      <RenderItem item={leaders.filter((leader) => leader.featured)[0]} />
+    </ScrollView>
+  );
 }
