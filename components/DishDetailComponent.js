ae1db83d281fd9ae75ee4cd57f14d309cf727573
Luis Locon <luis.locon@gmail.com>
2020-07-29 21:55:17 -0600

    [adding: A button to favorite a menu]

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
-import { Card } from "react-native-elements";
-import { DISHES } from '../shared/dishes'
+import { View, Text, ScrollView, FlatList } from "react-native";
+import { Card, Icon } from "react-native-elements";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
 
-const RenderDish = ({ dish }) => {
+const RenderDish = ({ dish, favorite, onPress }) => {
   if (dish != null) {
     return (
       <Card
@@ -11,6 +12,16 @@ const RenderDish = ({ dish }) => {
         image={require("./images/uthappizza.png")}
       >
         <Text style={{ margin: 10 }}>{dish.description}</Text>
+        <Icon
+          raised
+          reverse
+          name={favorite ? "heart" : "heart-o"}
+          type="font-awesome"
+          color="#f50"
+          onPress={() => {
+            favorite ? console.log("Already Favorite") : onPress();
+          }}
+        />
       </Card>
     );
   }
@@ -18,9 +29,55 @@ const RenderDish = ({ dish }) => {
   return <View></View>;
 };
 
+const RenderComments = ({ comments }) => {
+  const renderCommentItem = ({ item, index }) => {
+    return (
+      <ScrollView>
+        <View key={index} style={{ margin: 10 }}>
+          <Text style={{ fontSize: 14 }}>{item.comment}</Text>
+          <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+          <Text style={{ fontSize: 12 }}>
+            {"-- " + item.author + ", " + item.date}
+          </Text>
+        </View>
+      </ScrollView>
+    );
+  };
+  return (
+    <Card title="Comments">
+      <FlatList
+        data={comments}
+        renderItem={renderCommentItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </Card>
+  );
+};
+
 export default function DishDetailComponent({ route }) {
   const [dishes] = useState(DISHES);
-  const id = route.params.dishID ? route.params.dishID : null;
+  const [comments] = useState(COMMENTS);
+  const [favorites, setFavorites] = useState([]);
+  const id = route.params.dishID
+    ? route.params.dishID
+    : route.params.dishID == 0
+    ? 0
+    : null;
 
-  return <RenderDish dish={dishes[+id]} />;
+  const markFavorite = (dishID) => {
+    setFavorites(favorites.concat(dishID));
+  };
+
+  return (
+    <ScrollView>
+      <RenderDish
+        dish={dishes[+id]}
+        favorite={favorites.some((el) => el === id)}
+        onPress={() => markFavorite(id)}
+      />
+      <RenderComments
+        comments={comments.filter((comment) => comment.dishId === id)}
+      />
+    </ScrollView>
+  );
 }
